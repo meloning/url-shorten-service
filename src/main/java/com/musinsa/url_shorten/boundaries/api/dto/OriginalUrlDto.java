22229ab986409075ccd3793ecf2bdc259dbd963c
junsu <melon8372@gmail.com>
2021-08-30 02:47:57 +0900

    bugfix: Fix OriginalUrlDto.url empty case

--- src/main/java/com/musinsa/url_shorten/boundaries/api/dto/OriginalUrlDto.java
+++ src/main/java/com/musinsa/url_shorten/boundaries/api/dto/OriginalUrlDto.java
@@ -5,10 +5,13 @@ import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 public class OriginalUrlDto {
+    @NotBlank(message = "It cannot be empty or blank.")
     @URL(message = "The URL format is not correct.")
     private String url;
 }
